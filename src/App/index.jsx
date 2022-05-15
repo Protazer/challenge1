57bc80bf661178786bedae15a14849f82cc6ebae
Protazer <florian.huberdeau@laposte.net>
2022-05-15 13:24:18 +0200

    feat: feat folder structure and assets import

---
+++ src/App/index.jsx
@@ -0,0 +1,28 @@
+//Librairies import
+import React from 'react';
+import { createGlobalStyle } from 'styled-components';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+//Pages import
+import Home from '../pages/Home';
+
+function App() {
+  return (
+    <Router>
+      <GlobalStyle />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+      </Routes>
+    </Router>
+  );
+}
+
+const GlobalStyle = createGlobalStyle`
+  *{
+     margin:0;
+     padding:0;
+     box-sizing: border-box;
+  }
+`;
+
+export default App;
---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,8 @@
+//Librairies import
+import React from 'react';
+
+const Home = () => {
+  return <h1>Je suis la page d'accueil !</h1>;
+};
+
+export default Home;
